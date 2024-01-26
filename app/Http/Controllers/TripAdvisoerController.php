0bccb8aa019291fd14e99202c5edfb4afe7d09ba
Shinwari <sohailciit38@gmail.com>
2024-01-26 11:31:41 +0500

    change Api of tripadvisoer

--- app/Http/Controllers/TripAdvisoerController.php
+++ app/Http/Controllers/TripAdvisoerController.php
@@ -10,16 +10,24 @@ class TripAdvisoerController extends Controller
     public function getReviews()
     {
         $client = new Client(); // Initialize Guzzle client
-
-        $response = $client->request('GET', 'https://api.content.tripadvisor.com/api/v1/location/nearby_search?language=en', [
+        
+        $response = $client->request('GET', 'https://api.content.tripadvisor.com/api/v1/location/locationId/details?language=en&currency=USD&key=Pakistan', [
             'headers' => [
                 'API-Key' => env('TRIPADVISOR_API_KEY'),
-                // Add other necessary headers
-                // 'accept' => 'application/json',
+              'accept' => 'application/json',
             ],
-            // Add any other necessary options
-        ]);
+          ]);
+          
+          echo $response->getBody();
+        // $response = $client->request('GET', 'https://api.content.tripadvisor.com/api/v1/location/nearby_search?language=en', [
+        //     'headers' => [
+        //         'API-Key' => env('TRIPADVISOR_API_KEY'),
+        //         // Add other necessary headers
+        //         // 'accept' => 'application/json',
+        //     ],
+        //     // Add any other necessary options
+        // ]);
 
-        return json_decode($response->getBody()->getContents());
+        // return json_decode($response->getBody()->getContents());
     }
 }

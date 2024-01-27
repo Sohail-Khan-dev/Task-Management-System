ff285f32cc62ff1c8c7eb2d3a8b228cacd3f4df9
Shinwari <sohailciit38@gmail.com>
2024-01-27 20:49:22 +0500

    Add social Media links

--- resources/views/Layouts/footer.blade.php
+++ resources/views/Layouts/footer.blade.php
@@ -20,23 +20,14 @@
                     </li>
                 </ul>
             </div>
-
             <!-- Footer Column 2 -->
             <div class="w-full md:w-1/4 text-sm mb-6 md:mb-0">
                 <h5 class="uppercase mb-4 font-bold">Resources</h5>
                 <ul class="list-none">
-                    <li class="mb-2">
-                        <a href="#blog" class="hover:text-blue-200">Blog</a>
-                    </li>
-                    <li class="mb-2">
-                        <a href="#help" class="hover:text-blue-200">Help Center</a>
-                    </li>
-                    <li class="mb-2">
-                        <a href="#privacy" class="hover:text-blue-200">Privacy Policy</a>
-                    </li>
-                    <li class="mb-2">
-                        <a href="#terms" class="hover:text-blue-200">Terms of Service</a>
-                    </li>
+                    <li class="mb-2"> <a href="#blog" class="hover:text-blue-200">Blog</a>             </li>
+                    <li class="mb-2"><a href="#help" class="hover:text-blue-200">Help Center</a>       </li>
+                    <li class="mb-2"><a href="#privacy" class="hover:text-blue-200">Privacy Policy</a> </li>
+                    <li class="mb-2"><a href="#terms" class="hover:text-blue-200">Terms of Service</a> </li>
                 </ul>
             </div>
 
@@ -45,9 +36,20 @@
 
             <!-- Footer Social Links (Optional) -->
             <div class="w-full md:w-1/4 text-sm mb-6 md:mb-0">
-                <h5 class="uppercase mb-4 font-bold">Follow Us</h5>
-                <!-- Social Media Links Here -->
-            </div>
+    <h5 class="uppercase mb-4 font-bold">Follow Us</h5>
+    <ul class="list-none [&.*]]:text-white">
+        <li>
+            <a href="https://www.facebook.com/Sohailshinwar" target="_blank"  class="hover:text-gray-800">Facebook</a>
+        </li>
+        <li>
+            <a href="https://twitter.com/SohailKhan8338" target="_blank"  class="hover:text-gray-800">Twitter</a>
+        </li>
+        <li>
+            <a href="https://www.linkedin.com/in/sohailshinwari/" target="_blank"  class="hover:text-gray-800">LinkedIn</a>
+        </li>
+    </ul>
+</div>
+
         </div>
 
         <div class="border-t border-gray-700 mt-8 pt-6 text-center">
